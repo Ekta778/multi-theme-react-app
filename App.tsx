6e302c1b0d7e6925c8cfeeed58745cd0bc8c9994
Ekta Prakash <127758658+Ekta778@users.noreply.github.com>
2025-07-31 15:58:58 +0530

    Create  App.tsx

---
+++ App.tsx
@@ -0,0 +1,26 @@
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './context/ThemeContext';
+import { Header } from './components/Header';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <Router>
+        <Header />
+        <main className="pt-20 px-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
